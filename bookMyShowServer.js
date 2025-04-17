d33576ebde480361ad9ef1838ac189cdca856736
Aryan <aryansinghal207@gmail.com>
2025-04-17 10:38:44 +0530

    java

--- bookMyShowServer.js
+++ bookMyShowServer.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 const showsFile = path.join(__dirname, "shows.json");
-const bookingsFile = path.join(__dirname, "bookings.json");
+const bookingsFile = path.join(__dirname, "bookings.json"); 
 const citiesFile = path.join(__dirname, "cities.json");
 
 // File Operations
